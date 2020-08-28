82c97b78c2dcad625ae4749d2bfb5f91a24dfc67
kenny18alfaro <70359472+kenny18alfaro@users.noreply.github.com>
2020-08-27 22:10:41 -0600

    123
    proyecto

---
+++ operaciones.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tabla;
+
+
+public class operaciones {
+ 
+    private String datos;
+
+    public operaciones(String datos) {
+        this.datos = datos;
+    }
+
+
+
+
+    public String getDatos() {
+        return datos;
+    }
+
+    public void setDatos(String datos) {
+        this.datos = datos;
+    }
+    
+    
+}
